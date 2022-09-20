45b0bb8653a417ab63aad9add7e5ee011a182ab3
NatiG <nati.g5684@gmail.com>
2022-09-20 14:57:11 +0300

    Create message model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Message.create(text: 'Greeting1')
+Message.create(text: 'Greeting2')
+Message.create(text: 'Greeting3')
+Message.create(text: 'Greeting4')
+Message.create(text: 'Greeting5')
