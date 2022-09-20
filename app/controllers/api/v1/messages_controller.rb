f5ba8231b14adbdd1bb39842b3e2fe231f6259d6
NatiG <nati.g5684@gmail.com>
2022-09-21 02:09:14 +0300

    Install & fix linter erros

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -1,8 +1,8 @@
 class Api::V1::MessagesController < ApplicationController
-    def index 
-        @messages = Message.first.text;
-        render json: {
-            text: @messages
-        }
-    end
+  def index
+    @messages = Message.first.text
+    render json: {
+      text: @messages
+    }
+  end
 end
