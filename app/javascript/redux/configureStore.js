d21912ec78170cb63fc87d72bf86ebaa663ceefa
NatiG <nati.g5684@gmail.com>
2022-09-21 01:25:38 +0300

    Install redux thunk

--- app/javascript/redux/configureStore.js
+++ app/javascript/redux/configureStore.js
@@ -1,10 +1,10 @@
-import { combineReducers, legacy_createStore as createStore } from "redux";
+import { applyMiddleware, combineReducers, legacy_createStore as createStore } from "redux";
+import thunk from "redux-thunk";
 import greetingReducer from "./greeting/greeting";
 
 const rootReducer = combineReducers({
     greetingReducer
 })
 
-const store = createStore(rootReducer);
-
+const store = createStore(rootReducer, applyMiddleware(thunk));
 export default store;
